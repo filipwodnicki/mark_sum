f20713b435e5d1e5c66f854d0a1b0e57bdef0617
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-08-16 12:30:17 +0200

    restructure

--- mark_sum.py
+++ mark_sum.py
@@ -1,38 +1,65 @@
 import sys
 
+from bs4 import BeautifulSoup
+import requests
+
 from sumy.parsers.html import HtmlParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.utils import get_stop_words
 
-from bs4 import BeautifulSoup
-import requests
-
 LANGUAGE = "english"
 SENTENCES_COUNT = 3
 
-def get_all_images(url):
-	images = []
+def get_soup(url):
 	r = requests.get(url)
 	soup = BeautifulSoup(r.text, features="lxml")
-	for img in soup.findAll('img', width=True, height=True):
-		images.append(img.get('src'))
-	return images
+	return soup
+
+# def get_all_images(url):
+# 	images = []
+# 	for img in get_soup(url).findAll('img', width=True, height=True):
+# 		images.append(img.get('src'))
+# 	return images
+
+def get_h1(soup):
+	soup_h1 = soup.find('h1').get_text()
+	return soup_h1
+
+
+def get_img_after_h1(soup):
+	soup_h1 = soup.find('h1')
+	soup_img_url = soup_h1.find_next('img').get('src')
+	return soup_img_url
 
 if __name__ == '__main__':
 	url = sys.argv[1]
-	print("Type of url: ", type(url))
+	# print("Type of url: ", type(url))
 	print("url: ", str(url))
 	# url = "https://www.bbc.com/news/business-45105674"
+
+	soup = get_soup(url)
+	title = get_h1(soup)
+	print("Title: ", title)
+
+	featured_image_url = get_img_after_h1(soup)
+	print("img:", featured_image_url)
+
+
 	parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
 	stemmer = Stemmer(LANGUAGE)
 	summarizer = Summarizer(stemmer)
 	summarizer.stop_words = get_stop_words(LANGUAGE)
 
-	for sentence in summarizer(parser.document, SENTENCES_COUNT):
+	summary = summarizer(parser.document, SENTENCES_COUNT)
+
+	for sentence in summary:
 		print(sentence)
 
-	print ( get_all_images(url) )
+
+
+	
+	
 
 
